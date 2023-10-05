94ff8ebda122a50d2b616b6ecd8f9c8045a4fe68
kingbong131 <kingbong131@gmail.com>
2023-10-05 19:32:18 +0900

    [bongjune] : 2주차 출석과제

--- src/EventPrac.js
+++ src/EventPrac.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EventPrac = () => {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const onchangeMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const onClick = () => {
+    alert(username + ": " + message);
+    setUsername("");
+    setMessage("");
+  };
+
+  const pressEnter = (e) => {
+    if (e.key == "Enter") {
+      onClick();
+    }
+  };
   return (
     <div>
       <h1>이벤트 연습</h1>
       {/* input 연습 - type, name, placeholder, value, onChange, onKeyPress*/}
-
+      <input
+        type="text"
+        name="username"
+        placeholder="user명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="메시지 입력"
+        value={message}
+        onChange={onchangeMessage}
+      />
       {/* button */}
+      <button onClick={onClick}>확인</button>
     </div>
   );
 };
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,15 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //컴포넌트 생성
-const MyComponent = () => {
-  return <div>My First Component</div>;
+const MyComponent = ({ name, children }) => {
+  return (
+    <div>
+      My First Component
+      <h1>제 이름은 {name}</h1>
+      <h2>chirldren값은 {children} 입니다</h2>
+    </div>
+  );
 };
 
 //1. props 렌더링
 
 //2. defaultProps
 MyComponent.defaultProps = {
-  name: "기본 이름",
+  name: "default",
+};
+
+MyComponent.prototype = {
+  name: "default",
 };
 
 export default MyComponent;
